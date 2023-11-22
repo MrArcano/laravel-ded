a739e7f702a95ae71678270132fe97cfd2179df2
estherpinos <estherpinos1@gmail.com>
2023-11-22 16:51:26 +0100

    Add [layout]

---
+++ resources/views/home.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.main')
+
+@section('content')
+<h1 class="my-3 text-center ">D&D personaggi</h1>
+<table class="table table-dark mt-5">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Altezza</th>
+        <th scope="col">Peso</th>
+        <th scope="col">Classe armatura</th>
+        <th scope="col">FOR</th>
+        <th scope="col">DES</th>
+        <th scope="col">COS</th>
+        <th scope="col">INT</th>
+        <th scope="col">SAG</th>
+        <th scope="col">CAR</th>
+
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+
+      </tr>
+
+    </tbody>
+  </table>
+
+
+
+@endsection
