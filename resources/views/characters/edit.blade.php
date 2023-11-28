6a917e1facf9de77c8b98e7c548dc9782f261e08
MrArcano <salvatore-dario@hotmail.it>
2023-11-28 13:14:29 +0100

    [edit]: layout show

--- resources/views/characters/edit.blade.php
+++ resources/views/characters/edit.blade.php
@@ -21,11 +21,11 @@
             <div class="mb-3">
                 <label for="name" class="form-label">Name: </label>
                 <input
-                  type="text"
-                  class="form-control @error('name') is-invalid  @enderror"
-                  id="name"
-                  name="name"
-                  value="{{ old('name', $character->name)}}">
+                    type="text"
+                    class="form-control @error('name') is-invalid  @enderror"
+                    id="name"
+                    name="name"
+                    value="{{ old('name', $character->name)}}">
                 @error('name')
                     <span class="text-danger">{{ $message }}</span>
                 @enderror
@@ -33,10 +33,10 @@
             <div class="mb-3">
                 <label for="background" class="form-label">Background: </label>
                 <textarea
-                  class="form-control @error('background') is-invalid  @enderror"
-                  id="background"
-                  rows="3"
-                  name="background">{{ old('background', $character->background)}}</textarea>
+                    class="form-control @error('background') is-invalid  @enderror"
+                    id="background"
+                    rows="3"
+                    name="background">{{ old('background', $character->background)}}</textarea>
                 @error('background')
                     <span class="text-danger">{{ $message }}</span>
                 @enderror
@@ -46,11 +46,11 @@
                     <div class="mb-3">
                         <label for="height" class="form-label">Altezza: </label>
                         <input
-                          type="number"
-                          class="form-control @error('height') is-invalid  @enderror"
-                          id="height"
-                          name="height"
-                          value="{{ old('height', $character->height)}}">
+                            type="number"
+                            class="form-control @error('height') is-invalid  @enderror"
+                            id="height"
+                            name="height"
+                            value="{{ old('height', $character->height)}}">
                         @error('height')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
@@ -60,11 +60,11 @@
                     <div class="mb-3">
                         <label for="weight" class="form-label">Peso: </label>
                         <input
-                          type="number"
-                          class="form-control @error('weight') is-invalid  @enderror"
-                          id="weight"
-                          name="weight"
-                          value="{{ old('weight', $character->weight)}}">
+                            type="number"
+                            class="form-control @error('weight') is-invalid  @enderror"
+                            id="weight"
+                            name="weight"
+                            value="{{ old('weight', $character->weight)}}">
                         @error('weight')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
@@ -74,10 +74,10 @@
                     <div class="mb-3">
                         <label for="armour_class" class="form-label">Classe armatura:  </label>
                         <select
-                          class="form-control @error('armour_class') is-invalid  @enderror"
-                          name="armour_class"
-                          id="armour_class"
-                          value="{{ old('armour_class', $character->armour_class)}}">
+                            class="form-control @error('armour_class') is-invalid  @enderror"
+                            name="armour_class"
+                            id="armour_class"
+                            value="{{ old('armour_class', $character->armour_class)}}">
                             <option value="Leggera">Leggera</option>
                             <option value="Media">Media</option>
                             <option value="Pesante">Pesante</option>
@@ -92,11 +92,11 @@
             <div class="mb-3">
                 <label for="image" class="form-label">Image: </label>
                 <input
-                  type="text"
-                  class="form-control @error('image') is-invalid  @enderror"
-                  id="image"
-                  name="image"
-                  value="{{ old('image', $character->image)}}">
+                    type="text"
+                    class="form-control @error('image') is-invalid  @enderror"
+                    id="image"
+                    name="image"
+                    value="{{ old('image', $character->image)}}">
                 @error('image')
                     <span class="text-danger">{{ $message }}</span>
                 @enderror
@@ -107,11 +107,11 @@
                     <div class="mb-3">
                         <label for="FOR" class="form-label">FOR: </label>
                         <input
-                          type="number"
-                          class="form-control @error('FOR') is-invalid  @enderror"
-                          id="FOR"
-                          name="FOR"
-                          value="{{ old('FOR', $character->FOR)}}">
+                            type="number"
+                            class="form-control @error('FOR') is-invalid  @enderror"
+                            id="FOR"
+                            name="FOR"
+                            value="{{ old('FOR', $character->FOR)}}">
                         @error('FOR')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
@@ -121,11 +121,11 @@
                     <div class="mb-3">
                         <label for="DES" class="form-label">DES: </label>
                         <input
-                          type="number"
-                          class="form-control @error('DES') is-invalid  @enderror"
-                          id="DES"
-                          name="DES"
-                          value="{{ old('DES', $character->DES)}}">
+                            type="number"
+                            class="form-control @error('DES') is-invalid  @enderror"
+                            id="DES"
+                            name="DES"
+                            value="{{ old('DES', $character->DES)}}">
                         @error('DES')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
@@ -135,11 +135,11 @@
                     <div class="mb-3">
                         <label for="COS" class="form-label">COS: </label>
                         <input
-                          type="number"
-                          class="form-control @error('COS') is-invalid  @enderror"
-                          id="COS"
-                          name="COS"
-                          value="{{ old('COS', $character->COS)}}">
+                            type="number"
+                            class="form-control @error('COS') is-invalid  @enderror"
+                            id="COS"
+                            name="COS"
+                            value="{{ old('COS', $character->COS)}}">
                         @error('COS')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
@@ -149,11 +149,11 @@
                     <div class="mb-3">
                         <label for="INT" class="form-label">INT: </label>
                         <input
-                          type="number"
-                          class="form-control @error('INT') is-invalid  @enderror"
-                          id="INT"
-                          name="INT"
-                          value="{{ old('INT', $character->INT)}}">
+                            type="number"
+                            class="form-control @error('INT') is-invalid  @enderror"
+                            id="INT"
+                            name="INT"
+                            value="{{ old('INT', $character->INT)}}">
                         @error('INT')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
@@ -163,11 +163,11 @@
                     <div class="mb-3">
                         <label for="SAG" class="form-label">SAG: </label>
                         <input
-                          type="number"
-                          class="form-control @error('SAG') is-invalid  @enderror"
-                          id="SAG"
-                          name="SAG"
-                          value="{{ old('SAG', $character->SAG)}}">
+                            type="number"
+                            class="form-control @error('SAG') is-invalid  @enderror"
+                            id="SAG"
+                            name="SAG"
+                            value="{{ old('SAG', $character->SAG)}}">
                         @error('SAG')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
@@ -177,11 +177,11 @@
                     <div class="mb-3">
                         <label for="CAR" class="form-label">CAR: </label>
                         <input
-                          type="number"
-                          class="form-control @error('CAR') is-invalid  @enderror"
-                          id="CAR"
-                          name="CAR"
-                          value="{{ old('CAR', $character->CAR)}}">
+                            type="number"
+                            class="form-control @error('CAR') is-invalid  @enderror"
+                            id="CAR"
+                            name="CAR"
+                            value="{{ old('CAR', $character->CAR)}}">
                         @error('CAR')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -1,36 +1,76 @@
 @extends('layouts.main')
 
 @section('content')
+    <h1 class="text-center">Show</h1>
+    <div class="text-end">
+        <a class="btn btn-warning " href="{{ route('characters.edit', $character) }}"><i class="fa-solid fa-pencil"></i></a>
+    </div>
     <div class="show my-3">
-        <h1>Show - {{ $character->name }}</h1>
 
-        <div class="d-flex my-4">
-            <img src="{{ $character->image }}" alt="{{ $character->name }}">
-            <div class="p-3">
-                <p>{{ $character->background }}</p>
-                <p>Altezza: {{ $character->height }} | Peso: {{ $character->weight }} |Classe Armor: {{ $character->armour_class }}</p>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">FOR</th>
-                            <th scope="col">DES</th>
-                            <th scope="col">COS</th>
-                            <th scope="col">INT</th>
-                            <th scope="col">SAG</th>
-                            <th scope="col">CAR</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>{{ $character->FOR }}</th>
-                            <th>{{ $character->DES }}</th>
-                            <th>{{ $character->COS }}</th>
-                            <th>{{ $character->INT }}</th>
-                            <th>{{ $character->SAG }}</th>
-                            <th>{{ $character->CAR }}</th>
-                        </tr>
-                    </tbody>
-                </table>
+        <div class="row">
+            <div class="col">
+                <div class="image m-auto">
+                    <img src="{{ $character->image }}" alt="{{ $character->name }}">
+
+                    <span class="name-character"><strong>{{ $character->name }}</strong></span>
+
+                </div>
+            </div>
+            <div class="col">
+                <div class="p-3">
+                    <p><strong>Descrizione: </strong>{{ $character->background }}</p>
+                    <p><strong>Altezza: </strong>{{ $character->height }} | <strong>Peso: </strong>{{ $character->weight }} | <strong>Classe Armor: </strong>{{ $character->armour_class }}</p>
+                </div>
+                <div class="row">
+                    <div class="col-6 col-md-4 col-lg-2 my-3">
+                        <div class="attribute">
+                            <p class="val">
+                                <strong>{{ $character->FOR }}</strong>
+                            </p>
+                            <p class="text">FOR</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg-2 my-3">
+                        <div class="attribute">
+                            <p class="val">
+                                <strong>{{ $character->DES }}</strong>
+                            </p>
+                            <p class="text">DES</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg-2 my-3">
+                        <div class="attribute">
+                            <p class="val">
+                                <strong>{{ $character->COS }}</strong>
+                            </p>
+                            <p class="text">COS</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg-2 my-3">
+                        <div class="attribute">
+                            <p class="val">
+                                <strong>{{ $character->INT }}</strong>
+                            </p>
+                            <p class="text">INT</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg-2 my-3">
+                        <div class="attribute">
+                            <p class="val">
+                                <strong>{{ $character->SAG }}</strong>
+                            </p>
+                            <p class="text">SAG</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg-2 my-3">
+                        <div class="attribute">
+                            <p class="val">
+                                <strong>{{ $character->CAR }}</strong>
+                            </p>
+                            <p class="text">CAR</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
