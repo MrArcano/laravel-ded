1f41679f99fd863f3fd2bf85c466d52d5402b9dd
AnotherDebug <antonio.faddanno@gmail.com>
2023-12-12 18:15:37 +0100

    [ADD]: seeder skills

---
+++ database/seeders/CharactersSkillsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CharactersSkillsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
